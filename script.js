38a721f8e8948e1e11dfb618fa5d367b10e9e1a5
Bruno Serafini <bruno.serafini@pm.me>
2024-04-29 20:54:07 -0300

    feat: add hover effect to div squares

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ for (let i = 0; i < (16*16); i++) {
   const el = document.createElement("div");
   el.classList.add("el-grid");
 
+  el.addEventListener("mouseover", (e) => {
+    el.style["backgroundColor"] = "#000";
+  });
+
   document.querySelector("#container").appendChild(el);
 }
