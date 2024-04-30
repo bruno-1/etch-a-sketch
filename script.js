c40dfce1722e3b713c5e3374eab57900fd1f0ffd
Bruno Serafini <bruno.serafini@pm.me>
2024-04-29 21:47:54 -0300

    feat: add button to set number of squares on each side

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-for (let i = 0; i < (16*16); i++) {
-  const el = document.createElement("div");
-  el.classList.add("el-grid");
+const QTT_SQUARES_ON_EACH_SIDE_INITIAL = 16;
 
-  el.addEventListener("mouseover", (e) => {
-    el.style["backgroundColor"] = "#000";
-  });
+function changeQttSquaresOnEachSide(qttSquares) {
+  qttSquares = parseInt(qttSquares);
+  if (!qttSquares) qttSquares = QTT_SQUARES_ON_EACH_SIDE_INITIAL;
+  if (qttSquares > 100) qttSquares = 100;
 
-  document.querySelector("#container").appendChild(el);
+  while (document.querySelector("#container").firstChild) {
+    document.querySelector("#container").removeChild(
+      document.querySelector("#container").firstChild
+    );
+  }
+
+  for (let i = 0; i < (qttSquares*qttSquares); i++) {
+    const el = document.createElement("div");
+    el.style['width'] = (QTT_SQUARES_ON_EACH_SIDE_INITIAL/qttSquares) + 'rem';
+    el.style['height'] = (QTT_SQUARES_ON_EACH_SIDE_INITIAL/qttSquares) + 'rem';
+  
+    el.addEventListener("mouseover", (e) => {
+      el.style["backgroundColor"] = "#000";
+    });
+  
+    document.querySelector("#container").appendChild(el);
+  }
 }
+
+document.querySelector("#qtt-squares-side").addEventListener("click", (e) => {
+  const qtt = prompt("Enter the number of squares on each side");
+  changeQttSquaresOnEachSide(qtt);
+});
+
+changeQttSquaresOnEachSide(QTT_SQUARES_ON_EACH_SIDE_INITIAL);
